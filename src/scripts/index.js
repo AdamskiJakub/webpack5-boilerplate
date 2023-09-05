67e1a067331994dfbb83ffa8d802f6bd329e03ef
AdamskiJakub <105269736+AdamskiJakub@users.noreply.github.com>
2023-09-05 13:43:28 +0200

    Creating Navbar and Hero section plus styling slider

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,28 @@
-import '../styles/main.scss';
+import "../styles/main.scss";
+import logoImg from "../assets/logo.png";
+import mobileZoniferoLogo from "../assets/zonifero_logo.png";
+import globe from "../assets/globe.png";
+import hand from "../assets/hand.png";
+import apple from "../assets/apple_store_badge.png";
+import play from "../assets/google_store_badge.png";
+import problemsolving from "../assets/mob_roblem_solving.png";
 
-import { bindEvents, getJoke, setImage } from './getChuckNorrisJoke';
+// Rozwijana lista do zmiany języka na ikonce Globe
+const dropdownToggle = document.getElementById("dropdown-toggle");
+const dropdownContent = document.querySelector(".dropdown-content");
 
-getJoke()
-bindEvents()
-setImage()
+dropdownToggle.addEventListener("click", () => {
+  console.log("clicked: ");
+
+  const isDropdownVisible = dropdownContent.style.display === "block";
+
+  if (isDropdownVisible) {
+    dropdownContent.style.display = "none";
+    document.querySelector(".arrow-down").style.display = "inline-block";
+    document.querySelector(".arrow-up").style.display = "none";
+  } else {
+    dropdownContent.style.display = "block";
+    document.querySelector(".arrow-down").style.display = "none";
+    document.querySelector(".arrow-up").style.display = "inline-block";
+  }
+});
